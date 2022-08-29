02b1b101312d17f06e4459307eec78d4eda957b7
Toan <chan.misha95@mail.ru>
2022-08-29 18:03:17 +0300

    - Сверстал экран и связал его с CurrentLocationVC

--- MyLocations/CurrentLoactionViewController.swift
+++ MyLocations/CurrentLoactionViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class CurrentLoactionViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longtitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - Actions
+    @IBAction func getLocation() {
+        
     }
 
 
