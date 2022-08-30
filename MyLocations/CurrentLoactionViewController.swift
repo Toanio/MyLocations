92409c88d0a2289c33b30a105e263eb9bee9cf9b
Toan <chan.misha95@mail.ru>
2022-08-30 17:17:16 +0300

    - Добавил кэширование координат

--- MyLocations/CurrentLoactionViewController.swift
+++ MyLocations/CurrentLoactionViewController.swift
@@ -38,8 +38,23 @@ class CurrentLoactionViewController: UIViewController, CLLocationManagerDelegate
          let newLocation = locations.last!
          print("didUpdateLocations \(newLocation)")
         
-        location = newLocation
-        lastLocationError = nil
+        if newLocation.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        
+        if newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+            lastLocationError = nil
+            location = newLocation
+        }
+        
+        if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+            print("*** We're done!")
+            stopLocationManager()
+        }
         updateLabels()
     }
 
