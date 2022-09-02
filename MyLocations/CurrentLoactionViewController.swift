a85124f49444c70f48cba1a1a8eea73049888bee
Toan <chan.misha95@mail.ru>
2022-09-02 18:47:22 +0300

    - Добавил новую View

--- MyLocations/CurrentLoactionViewController.swift
+++ MyLocations/CurrentLoactionViewController.swift
@@ -96,6 +96,14 @@ class CurrentLoactionViewController: UIViewController, CLLocationManagerDelegate
             }
         }
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by c.toan on 02.09.2022.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longtitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+     
+    @IBAction func close() {
+        navigationController?.popViewController(animated: true)
+    }
+}
