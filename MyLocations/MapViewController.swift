92706cb09daed29d19d6a61a523dee002011b282
Toan <chan.misha95@mail.ru>
2022-09-11 09:11:55 +0300

    - Добавил показ Локаций на карте

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -13,7 +13,23 @@ class MapViewController: UIViewController {
     @IBOutlet var mapView: MKMapView!
     
     var managedObjectContext: NSManagedObjectContext!
+    var locations = [Location] ()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLocations()
+    }
+    func updateLocations() {
+        mapView.removeAnnotations(locations)
+        
+        let entity = Location.entity()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations)
+    }
     @IBAction func showUser() {
         let region = MKCoordinateRegion(
             center: mapView.userLocation.coordinate,
